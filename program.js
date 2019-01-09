3021be831490fca5f440b9c2f907eb9ee092cea7
Radosław Biesek <radoslaw.biesek@gmail.com>
2019-01-09 21:15:50 +0100

    Add instructions

--- program.js
+++ program.js
@@ -1,17 +1,23 @@
 process.stdin.setEncoding('utf-8');
-process.stdin.on('readable', function() {
-
-    process.stdout.write('Node version: ' + process.versions.node + '\n');
-    process.stdout.write('OS language: ' + process.env.LANG + '\n');
-
+process.stdin.on('readable', function() {  
+    
     var input = process.stdin.read();
     if(input != null) {
         var instruction = input.toString().trim();
-        if( instruction === '/exit') {
-            process.stdout.write('Quitting app!\n');
-            process.exit();
+        switch (instruction) {
+            case '/version':
+                process.stdout.write('Node version: ' + process.versions.node + '\n');
+                break;
+            case '/lang':
+                process.stdout.write('OS language: ' + process.env.LANG + '\n');
+                break;
+            case '/exit':
+                process.stdout.write('Quitting app!\n');
+                process.exit();
+            default:
+                process.stderr.write('Wrong instruction\n');
+                break;
         }
-        process.stderr.write('Wrong instruction\n');
     }
-    
+
 });
